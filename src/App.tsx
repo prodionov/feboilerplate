2b120084fe5c6ef947f4bb5dafb43628d03eee2c
pavel <rpavel@Pavels-MBP.home>
2024-09-13 13:45:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Component from "./components/Component";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello world</p>
+        <Component />
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    hot: true,
+    historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    port: 3000,
+  },
+  entry: ["./src/index.tsx"],
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(bmp|png|svg|jpg|jpeg|gif|webp)$/i,
+        exclude: /node_modules/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
